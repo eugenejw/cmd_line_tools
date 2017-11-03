6d5e336f2c3dc484db08801b735276b9dbce764e
Weihan Jiang <weihan.github@gmail.com>
2017-11-03 12:18:06 -0700

    updated

--- compress_all.py
+++ compress_all.py
@@ -1,14 +1,64 @@
 import os
 from subprocess import call
 
-CMD = "7z a secure.7z * -pTEST"
-PASSWD = "TEST"
+class FileVaultManager(object):
+    def __init__(self):
+        self.dir = dir
+        self.targetDir = targetDir
+        self.passwd = "Test"
 
-fileNames = os.listdir('.')
-fileNames = filter(lambda x: not x.startswith('.'), fileNames)
+    def _getFileNames(self, dir=self.dir):
+        return os.listdir(dir)
 
-for fName in fileNames:
-    archiveName = fName.split('.')[0] + ".archive"
-    print "Compressing {} as {}...".format(fName, archiveName)
-    call(["7z", "a", archiveName, fName, "-p" + PASSWD])
+    def compressAll(self, dir=".", target_dir="./output/", passwd=self.passwd):
+        """
+        compress all files and directories
+        :return: void
+        """
+        target_dir = os.path.abspath(target_dir)
+        if not os.path.exists(target_dir):
+            os.makedirs(directory)
+        file_names = self._getFileNames(dir)
+        file_names = filter(lambda x: not x.startswith('.'), file_names)
+        for file in file_names:
+            if os.path.isfile(file) and os.stat(file).st_size < 10000000:
+                continue
+            archive_name = fName.split('.')[0] + ".archive"
+            print "Compressing {} as {}...".format(file, archive_name)
+            call(["7z", "a", archive_name, file, "-p" + passwd])
+            print "Moving zipped file {} to folder {}".format(archive_name, target_dir)
+            call(["mv", archive_name, target_dir])
+
+    def decompressAll(self, dir=".", target_dir="./output/"):
+        target_dir = os.path.abspath(target_dir)
+        if not os.path.exists(target_dir):
+            os.makedirs(directory)
+        file_names = self._getFileNames(dir)
+        file_names = filter(lambda x: x.endswith('.archive'), file_names)
+        for file in file_names:
+            print "De-compressing {} ...".format(file)
+            call(["7z", "a", file, "-p" + PASSWD])
+            print "Removing zipped file {}".format(file)
+            call(["rm", "-rf", file])
+
+    def flatAll(self, dir=".", target_dir="."):
+        """
+        Flat all files to parent dir
+        :param dir:
+        :return: void
+        """
+        target_dir = os.path.abspath(target_dir)
+        self._flatAll(dir, target_dir)
+
+
+    def _flatAll(self, dir, target_dir):
+        file_names = self._getFileNames(dir)
+        for file in file_names:
+            if os.path.isfile(file):
+                if os.stat(file).st_size >= 10000000: # >= 10M
+                    print "Moving {} to {}...".format(file, target_dir)
+                    call(["mv", file, target_dir])
+            if os.path.isdir(file):
+                print "Jumping to sub dir {}...".format(file)
+                self._flatAll(dir+file, target_dir)
 
